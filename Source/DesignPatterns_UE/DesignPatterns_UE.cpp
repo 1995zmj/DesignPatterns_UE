9b272193f319ceef01a50593ab364b318515fe9b
1995zmj <784178252@qq.com>
2021-05-17 20:48:02 +0800

    初始化

---
+++ Source/DesignPatterns_UE/DesignPatterns_UE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DesignPatterns_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DesignPatterns_UE, "DesignPatterns_UE" );
